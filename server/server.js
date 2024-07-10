b1469391a3d767a7ad7d669fcd8e6ef0effee669
Joel Azeta <ajfizzle310@outlook.com>
2024-07-09 22:16:01 -0500

    Learn to code -testing fix with filepath issue on Render

--- server/server.js
+++ server/server.js
@@ -1,12 +1,17 @@
-const express = require('express');
-
+const express = require("express");
+const path = require("path");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-app.use(express.static('../client/dist'));
+app.use(express.static(path.resolve(__dirname, "..", "client", "dist")));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-require('./routes/htmlRoutes')(app);
+require("./routes/htmlRoutes")(app);
+
+// Fallback route to serve index.html
+app.get("*", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "..", "client", "dist", "index.html"));
+});
 
 app.listen(PORT, () => console.log(`Now listening on port: ${PORT}`));
